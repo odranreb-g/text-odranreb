ac1be75866411041d8d71f7725c881c4c6a5ef4c
Bernardo Gomes <bgomesdeabreu@gmail.com>
2019-09-14 07:16:38 -0300

    Added step to remove stop words.

--- text_handler.py
+++ text_handler.py
@@ -1,6 +1,8 @@
 import string
 import attr
 
+from text_handler_utils import stop_words_portuguese
+
 # from nltk.tokenize import TreebankWordTokenizer
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
@@ -15,7 +17,7 @@ class TextHandler:
 
         the sw is abreviation of single word.
 
-        every word will be converted to lower case. s
+        every word will be converted to lower case.
     """
 
     texts = attr.ib()
@@ -29,8 +31,12 @@ class TextHandler:
         if not all([isinstance(v, str) for v in value]):
             raise ValueError("Every item inside the list must me string.")
 
+    def _remove_stop_words(self, words):
+        return [word for word in words if word not in stop_words_portuguese]
+
     def sw_vocabulary(self):
         words = word_tokenize(" ".join(self.texts), language="portuguese")
+        words = self._remove_stop_words(words)
         words = [word.lower() for word in words]
         words = set(words)
         words = words - set(string.punctuation)
@@ -42,6 +48,7 @@ class TextHandler:
 
     def sw_frequency_distribution(self):
         words = word_tokenize(" ".join(self.texts), language="portuguese")
+        words = self._remove_stop_words(words)
         words = [word.lower() for word in words]
         words = [word for word in words if word not in string.punctuation]
         words = list(words)
@@ -50,3 +57,4 @@ class TextHandler:
         freq_dist = FreqDist(words)
 
         return [freq for freq in freq_dist.values()]
+
--- text_handler_test.py
+++ text_handler_test.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import inspect
 import pytest
@@ -46,9 +45,11 @@ class TestSwVocabularyMethod:
 
     def test_if_return_list_with_ignored_punctuation(self):
         text_handler = TextHandler(
-            ["bernardo gomes; abreu. a! a# a% a$ a' a& a) a( a, a; a: a< a? a> a@ a[ a] a` a{ a} "]
+            [
+                "bernardo gomes; abreu. yasmine! yasmine# yasmine% yasmine$ yasmine' yasmine& yasmine) yasmine( yasmine, yasmine; yasmine: yasmine< yasmine? yasmine> yasmine@ yasmine[ yasmine] yasmine` yasmine{ yasmine} "
+            ]
         )
-        assert text_handler.sw_vocabulary() == ["a", "abreu", "bernardo", "gomes"]
+        assert text_handler.sw_vocabulary() == ["abreu", "bernardo", "gomes", "yasmine"]
 
     def test_if_return_list_with_ignored_case(self):
         text_handler = TextHandler(["bernardo GOMES; Abreu."])
@@ -57,6 +58,27 @@ class TestSwVocabularyMethod:
     def test_if_return_list_when_the_params_is_multiple_texts(self):
         text_handler = TextHandler(
             ["bernardo GOMES; Abreu.", "YASMINE", "Melo", "Costa", "Leonardo Gomes Abreu"]
+        )
+        assert text_handler.sw_vocabulary() == [
+            "abreu",
+            "bernardo",
+            "costa",
+            "gomes",
+            "leonardo",
+            "melo",
+            "yasmine",
+        ]
+
+    def test_if_return_list_without_stop_words(self):
+        text_handler = TextHandler(
+            [
+                "bernardo GOMES; de Abreu.",
+                "YASMINE",
+                "de",
+                "Melo",
+                "Costa",
+                "Leonardo Gomes de Abreu",
+            ]
         )
         assert text_handler.sw_vocabulary() == [
             "abreu",
