a8596b35f00a2997c026712bcb1245863b1c2161
Bernardo Gomes <bgomesdeabreu@gmail.com>
2019-09-15 07:14:52 -0300

    Refatored test of schema

--- backend/app/api_v1/tests/test_schemas.py
+++ backend/app/api_v1/tests/test_schemas.py
@@ -5,44 +5,41 @@ from app.api_v1.schemas import SendTextSchema, VocabularySchema
 
 
 class TestSendTextSchema:
+    @pytest.fixture(autouse=True)
+    def setUpAndTearDown(self):
+        self.schema = SendTextSchema()
+
     def test_validate_should_throw_expectio_when_validate_wrong_data(self):
         with pytest.raises(ValidationError):
             data = {"country": "brazil"}
-            schema = SendTextSchema()
-            schema.load(data)
+            self.schema.load(data)
 
     def test_validate_should_create_a_json_when_is_passed_a_valide_data(self):
         data = {"text": "brazil is cool"}
-        schema = SendTextSchema()
-        new_data = schema.load(data)
+        new_data = self.schema.load(data)
         assert new_data == data
 
     def test_validate_should_create_a_json_when_is_passed_a_valide_field_and_invalid_field(self):
         data = {"text": "brazil is cool", "another": "cool"}
-        schema = SendTextSchema()
-        new_data = schema.load(data)
+        new_data = self.schema.load(data)
         del data["another"]
         assert new_data == data
 
     def test_dump_should_create_a_json_with_id_and_text(self):
         data = {"text": "brazil is cool", "id": 2}
-
-        schema = SendTextSchema()
-        new_data = schema.dump(data)
+        new_data = self.schema.dump(data)
         assert data == new_data
 
     def test_dump_should_create_a_json_with_id_and_text_and_another_text(self):
         data = {"text": "brazil is cool", "id": 2, "another": "teste"}
-
-        schema = SendTextSchema()
-        new_data = schema.dump(data)
+        new_data = self.schema.dump(data)
         del data["another"]
         assert data == new_data
 
 
 class TestVocabularySchema:
     @pytest.fixture(autouse=True)
-    def data_remove_db_test(self):
+    def setUpAndTearDown(self):
         self.schema = VocabularySchema()
 
     def test_should_be_instace_of_schema(self):
