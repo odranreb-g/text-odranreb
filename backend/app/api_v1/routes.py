3b6783d6925da8547ba1fc33116e8674171922d9
Bernardo Gomes <bgomesdeabreu@gmail.com>
2019-09-15 07:16:53 -0300

    Created the endpoint IsolatedVocabularyApi

--- backend/app/api_v1/routes.py
+++ backend/app/api_v1/routes.py
@@ -1,9 +1,10 @@
 from flask import request
 from flask_restplus import Resource, Api
 from marshmallow.exceptions import ValidationError
-from app.api_v1.schemas import SendTextSchema
+from app.api_v1.schemas import SendTextSchema, VocabularySchema
 from app.models import Text
 from app.extensions import db
+from app.text_handler.text_handler import TextHandler
 from . import api_bp_v1
 
 api_restfull = Api(api_bp_v1)
@@ -32,3 +33,14 @@ class SendTextAPI(Resource):
         except ValidationError as error:
             return error.messages, 400
 
+
+@api_restfull.route("/isolated-vocabulary")
+class IsolatedVocabularyApi(Resource):
+    def get(self):
+        list_of_texts = [text for text, in db.session.query(Text.text).order_by(Text.id).all()]
+        text_handler = TextHandler(list_of_texts)
+
+        vocabulary = {"vocabulary": text_handler.sw_vocabulary()}
+        schema = VocabularySchema()
+
+        return schema.load(vocabulary), 200
--- backend/test_func/test_app.py
+++ backend/test_func/test_app.py
@@ -2,20 +2,16 @@ from http import HTTPStatus
 import json
 from flask import url_for
 import pytest
-
-# from app import flaskr
-
-
-# @pytest.fixture
-# def client():
-#     flaskr.app.config["TESTING"] = True
-
-#     with flaskr.app.test_client() as client:
-#         yield client
+from app.extensions import db
+from app.models import Text
 
 
 @pytest.mark.usefixtures("client_class")
 class TestEndpointSendTextAPI:
+    @pytest.fixture(autouse=True)
+    def data_remove_db_test(self):
+        db.session.query(Text).delete()
+
     def test_endpoint_send_text_with_valid_data(self, client):
         data = {"text": "new text to server"}
         rv = client.post(url_for("api.send_text_api"), data=json.dumps(data))
@@ -32,4 +28,81 @@ class TestEndpointSendTextAPI:
         assert rv.status_code == HTTPStatus.BAD_REQUEST
 
         assert rv.get_json() == {"text": ["Missing data for required field."]}
+
+    def test_check_methods(self, client):
+        rv = client.get(url_for("api.send_text_api"))
+        assert rv.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+        rv = client.put(url_for("api.send_text_api"))
+        assert rv.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+        rv = client.delete(url_for("api.send_text_api"))
+        assert rv.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+        rv = client.patch(url_for("api.send_text_api"))
+        assert rv.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+
+@pytest.mark.usefixtures("client_class")
+class TestIsolatedVocabularyAPI:
+    @pytest.fixture(autouse=True)
+    def data_remove_db_test(self):
+        db.session.query(Text).delete()
+
+    def test_check_methods(self, client):
+        rv = client.post(url_for("api.isolated_vocabulary_api"))
+        assert rv.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+        rv = client.put(url_for("api.isolated_vocabulary_api"))
+        assert rv.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+        rv = client.delete(url_for("api.isolated_vocabulary_api"))
+        assert rv.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+        rv = client.patch(url_for("api.isolated_vocabulary_api"))
+        assert rv.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+    def test_get_vocabulary_with_zero_texts(self, client):
+        response = client.get(url_for("api.isolated_vocabulary_api"))
+        assert response.status_code == HTTPStatus.OK
+        assert response.get_json() == {"vocabulary": []}
+
+    def test_get_vocabulary_with_one_text(self, client):
+        text = Text()
+        text.text = "Just a simple text"
+        db.session.add(text)
+        db.session.commit()
+
+        response = client.get(url_for("api.isolated_vocabulary_api"))
+        assert response.status_code == HTTPStatus.OK
+        assert response.get_json() == {"vocabulary": ["just", "simple", "text"]}
+
+    def test_get_vocabulary_with_multiple_text(self, client):
+        text = Text()
+        text.text = "Um simples teste"
+        db.session.add(text)
+        db.session.flush()
+
+        text = Text()
+        text.text = "Pytest é a melhor biblioteca de testes"
+        db.session.add(text)
+        db.session.flush()
+
+        text = Text()
+        text.text = "Eu estou utilizando Flask framework"
+        db.session.add(text)
+        db.session.flush()
+
+        db.session.commit()
+
+        response = client.get(url_for("api.isolated_vocabulary_api"))
+        assert response.status_code == HTTPStatus.OK
+        assert response.get_json() == {
+            "vocabulary": [
+                "simples",
+                "teste",
+                "pytest",
+                "melhor",
+                "biblioteca",
+                "testes",
+                "estou",
+                "utilizando",
+                "flask",
+                "framework",
+            ]
+        }
 
